3ff452b537c368f5a49283a34cbcb91d7e716651
Prince Paulson <prince.paulson@ericsson.com>
2020-05-24 19:22:19 +0100

    test 2 commit 2

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -7,3 +7,4 @@ CREATE TABLE note (
 
 INSERT INTO notepad.note VALUES (1, 'What is Cassandra?', 'Test 2 commit Cassandra is a NoSQL database that belongs to the Column Family NoSQL database category.');
 INSERT INTO notepad.note VALUES (1, 'What is Cassandra?', 'Test 2 commit2 Cassandra is a NoSQL database that belongs to the Column Family NoSQL database category.');
+INSERT INTO notepad.note VALUES (1, 'What is Cassandra?', 'Test 2 commit3 Cassandra is a NoSQL database that belongs to the Column Family NoSQL database category.');
